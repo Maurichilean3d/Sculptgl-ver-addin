62e97e8e9c32b2e214b4827f8a16723812d1b853
Maurichilean3d <Maurichilean3d@users.noreply.github.com>
2026-01-12 23:17:51 -0300

    Add files via upload

---
+++ src/files/Export.js
@@ -0,0 +1,22 @@
+import ExportOBJ from 'files/ExportOBJ';
+import ExportSGL from 'files/ExportSGL';
+import ExportPLY from 'files/ExportPLY';
+import ExportSTL from 'files/ExportSTL';
+import ExportGLB from 'files/ExportGLB'; // <--- Nuevo soporte GLB
+import ExportSketchfab from 'files/ExportSketchfab';
+import ExportSculpteo from 'files/ExportSculpteo';
+import ExportMaterialise from 'files/ExportMaterialise';
+
+var Export = {};
+Export.exportOBJ = ExportOBJ.exportOBJ;
+Export.exportSGL = ExportSGL.exportSGL;
+Export.exportAsciiPLY = ExportPLY.exportAsciiPLY;
+Export.exportBinaryPLY = ExportPLY.exportBinaryPLY;
+Export.exportAsciiSTL = ExportSTL.exportAsciiSTL;
+Export.exportBinarySTL = ExportSTL.exportBinarySTL;
+Export.exportGLB = ExportGLB.exportGLB; // <--- Nuevo soporte GLB
+Export.exportSketchfab = ExportSketchfab.exportSketchfab;
+Export.exportSculpteo = ExportSculpteo.exportSculpteo;
+Export.exportMaterialise = ExportMaterialise.exportMaterialise;
+
+export default Export;
